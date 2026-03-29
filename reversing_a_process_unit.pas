d621f3efd04f473a083e3da3135ad6a6c1ebd3bd
hapepo23 <238172935+hapepo23@users.noreply.github.com>
2026-03-29 15:13:17 +0200

    update 2026-03-29 15:13 CEST

--- reversing_a_process_unit.pas
+++ reversing_a_process_unit.pas
@@ -15,7 +15,6 @@ function Decode(s: string): string;
 implementation
 
 uses
-  StrUtils,
   SysUtils;
 
 function Decode(s: string): string;
@@ -23,7 +22,7 @@ const
   orda = Ord('a');
   cc = 26;
 var
-  i, l, pos, num, mi, y: integer;
+  i, l, pos, num, mi: integer;
 begin
   l := Length(s);
   pos := 1;
@@ -31,16 +30,20 @@ begin
     Inc(pos);
   if (pos = 1) or (pos > l) then
     Exit('Impossible to decode');
-  num := StrToInt(LeftStr(s, pos - 1));
+  num := StrToInt(LeftStr(s, pos - 1)) mod cc;
   mi := -1;
   for i := 1 to cc - 1 do
     if ((num * i) mod cc) = 1 then
+    begin
       mi := i;
+      Break;
+    end;
   if mi = -1 then
     Exit('Impossible to decode');
-  Result := '';
+  SetLength(Result, l - pos + 1);
   for i := pos to l do
-    Result := Result + Chr((((((Ord(s[i]) - orda) * mi) mod cc) + cc) mod cc) + orda);
+    Result[i - pos + 1] :=
+      Chr((((((Ord(s[i]) - orda) * mi) mod cc) + cc) mod cc) + orda);
 end;
 
 end.
