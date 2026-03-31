1995b84439b7b64431f40fc3fc9297afc2983315
hapepo23 <238172935+hapepo23@users.noreply.github.com>
2026-03-31 17:52:46 +0200

    update 2026-03-31 17:52 CEST

--- integer_partitions_unit.pas
+++ integer_partitions_unit.pas
@@ -26,6 +26,30 @@ var
   found: boolean;
   med, avg: double;
 begin
+  { Summary of the algorithm...
+  We store a partition in an array:
+  p[0], p[1], ..., p[k-1]
+  k = current length, always: p[0] ≥ p[1] ≥ ... ≥ p[k-1] and Sum(p) = n
+  Each step transforms the current partition into the next one.
+  Start with: p[0] = n and k = 1 
+  Loop:
+    Calculate product of current partition
+    Check if product already exists, if not, add it to "prod"
+    Modify partition slightly to get the next one:
+    Step 1: Find what can be changed
+      We scan from the right and remove all trailing 1s
+      because [... , 1,1,1] cannot be reduced further at the end.
+      So we: Count them into "rem", and shrink the partition
+    Step 2: Reduce the pivot
+      Take the rightmost number > 1 and decrease it
+      This creates the “next smaller” partition structure.
+    Step 3: Redistribute the remainder
+      We rebuild the rest using the largest allowed values
+      Constraint: new elements ≤ p[k-1]
+      Why? To maintain non-increasing order and avoid duplicates.
+  Repeat until done (all elements are 1 → stop)
+  Sort "prod" array
+  calculate range, average and median and store it in "result" }
   p := [];
   prod := [];
   lprod := 0;
