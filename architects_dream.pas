a532bcf882015d39164b4e4b090259283d586664
hapepo23 <238172935+hapepo23@users.noreply.github.com>
2026-04-26 22:22:49 +0200

    update 2026-04-26 22:22 CEST

---
+++ architects_dream.pas
@@ -0,0 +1,35 @@
+{
+6 kyu
+Architect's Dream: drawing curves, observing, thinking
+https://www.codewars.com/kata/5db19d503ec3790012690c11
+}
+
+program architects_dream;
+
+{$mode objfpc}{$H+}
+
+uses
+  SysUtils,
+  architects_dream_unit;
+
+  procedure DoTest(n, w: int64; Expected: double);
+  var
+    Actual: double;
+  begin
+    Actual := Weight(n, w);
+    writeln('n       : ', n);
+    writeln('w       : ', w);
+    writeln('Expected: ', Format('%.14g', [Expected]));
+    writeln('Actual  : ', Format('%.14g', [Actual]));
+    if Abs(Expected - Actual) < 1e-5 then
+      writeln('-> OK', LineEnding)
+    else
+      writeln('-> FAIL', LineEnding);
+  end;
+
+begin
+  DoTest(26, 4037, 693.319138109800);
+  DoTest(23, 2997, 514.708312339626);
+  DoTest(14, 4456, 765.278691953673);
+  DoTest(38, 2444, 419.735440559909);
+end.
---
+++ architects_dream_unit.pas
@@ -0,0 +1,34 @@
+{
+6 kyu
+Architect's Dream: drawing curves, observing, thinking
+https://www.codewars.com/kata/5db19d503ec3790012690c11
+}
+
+unit architects_dream_unit;
+
+{$mode objfpc}{$H+}
+
+interface
+
+function Weight(n, w: int64): double;
+
+implementation
+
+function Weight(n, w: int64): double;
+const
+  eminus2 = exp(-2.0);
+var
+  area: double = 0.14849853757254047;
+  areasum: double;
+  i: int64;
+begin
+  areasum := area;
+  for i := 0 to n - 1 do
+  begin
+    area := area * eminus2;
+    areasum += area;
+  end;
+  Result := areasum * w;
+end;
+
+end.
